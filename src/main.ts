341ee7c7615303d7c6e244caba1a53e129d0de2b
Paul Vasile <paul.vasile@ici.ro>
2022-05-23 12:21:19 +0300

    Temp removed CSRF

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import helmet from 'helmet';
 import { AppModule } from './app.module';
-import * as csurf from 'csurf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -23,7 +22,6 @@ async function bootstrap() {
 
   app.use(helmet());
   app.enableCors();
-  app.use(csurf());
 
   await app.listen(3000);
 }
